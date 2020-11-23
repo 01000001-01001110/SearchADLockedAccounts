c1652cfe13912c3f07bd81c61a40a3afac9fcbea
Alan Newingham <anewingham@sfb.osaa.net>
2020-11-23 15:19:53 -0500

    Create SearchADLockedAccounts.ps1
    Commit for final in Automate & Deploy

---
+++ SearchADLockedAccounts.ps1
@@ -0,0 +1,10 @@
+<#
+Search for and unlock all AD accounts that are locked. After first outputting their data in the console for review.
+#>
+Import-Module ActiveDirectory
+$locked = Search-ADAccount –LockedOut
+foreach ($l in $locked)
+    {
+        Write-Host $locked.Name is locked.
+    }
+Search-ADAccount -LockedOut | Unlock-ADAccount
